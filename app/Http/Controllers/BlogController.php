5f71c7ae7fe4bf9eb48e218db760f16f3de59a0a
johnfedoruk <johnny@johnfedoruk.ca>
2016-11-07 09:14:30 +0000

    Fixed error in viewing correct order of posts in welcome page and archive because db seeding on fast machine caused misalignment of ids and dates

--- app/Http/Controllers/BlogController.php
+++ app/Http/Controllers/BlogController.php
@@ -9,7 +9,7 @@ use App\Post;
 class BlogController extends Controller
 {
   public function getIndex() {
-    $posts = Post::orderBy("created_at","desc")->paginate(10);
+    $posts = Post::orderBy("id","desc")->paginate(10);
     return view('blog.index')->withPosts($posts)->withLen(255);
   }
   public function getSingle($slug) {
